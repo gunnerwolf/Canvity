1e3ab77368d92b4c914ab5e11216376f57b749a8
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-07-02 13:48:08 +0100

    Added public access modifier to FilePath getter

--- src/Canvity/Assets/Asset.ts
+++ src/Canvity/Assets/Asset.ts
@@ -1,7 +1,7 @@
 namespace Canvity.Assets {
     export abstract class Asset {
         protected filePath: string;
-        get FilePath(): string { return this.filePath; }
+        public get FilePath(): string { return this.filePath; }
 
         public constructor(path: string) {
             this.filePath = path;
