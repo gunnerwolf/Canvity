1e26f6da47b12bf1a9f9f31d74ddd0d9e78dc380
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-07-02 13:48:25 +0100

    Added override of Transform getter for CanvasComponents

--- src/Canvity/Component/CanvasComponent.ts
+++ src/Canvity/Component/CanvasComponent.ts
@@ -3,6 +3,8 @@ namespace Canvity.Component {
         protected canvasObject: CanvasObject;
         public get CanvasObject(): CanvasObject { return this.canvasObject; }
         public set CanvasObject(obj: CanvasObject) { if (!this.canvasObject) this.canvasObject = obj; }
+
+        public get Transform(): Transform { return this.canvasObject.Transform; }
         
         public Draw(time: Util.Time, ctx: CanvasRenderingContext2D): void {}
         public Update(time: Util.Time): void {}
