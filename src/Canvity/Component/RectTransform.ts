94a694ef94355ba7c0596146d71af89644d8c7f0
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-07-03 09:12:54 +0100

    Added RectTransform

---
+++ src/Canvity/Component/RectTransform.ts
@@ -0,0 +1,19 @@
+namespace Canvity.Component {
+    export class RectTransform extends Transform {
+        private size: Util.Vector2;
+        public get Size(): Util.Vector2 { return this.size; }
+        public set Size(val: Util.Vector2) { this.size = val; }
+
+        public get Rectangle(): Util.Rect { return new Util.Rect(this.LocalPosition.X, this.LocalPosition.Y, this.Size.X, this.Size.Y); }
+        public set Rectangle(val: Util.Rect) {
+            this.LocalPosition = new Util.Vector2(val.X, val.Y);
+            this.Size = new Util.Vector2(val.W, val.H);
+        }
+
+        public constructor() {
+            super();
+
+            this.Size = new Util.Vector2();
+        }
+    }
+}
