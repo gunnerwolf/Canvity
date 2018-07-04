7513993b643239176aa208050696fcbaf9664770
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-07-04 14:51:35 +0100

    Changed CanvityEvent to be type-agnostic

--- src/Canvity/Events/CanvityEvent.ts
+++ src/Canvity/Events/CanvityEvent.ts
@@ -1,23 +1,23 @@
 namespace Canvity.Events {
-    export class CanvityEvent<T> {
-        private listeners: Array<(value: T) => void>;
+    export class CanvityEvent {
+        private listeners: Array<(values: Array<any>) => void>;
 
         public constructor() {
-            this.listeners = new Array<(value: T) => void>();
+            this.listeners = new Array<(values: Array<any>) => void>();
         }
 
-        public AddEventListener(f: (value: T) => void): void {
-            this.listeners.push(f);
+        public AddEventListener(listener: (values: Array<any>) => void): void {
+            this.listeners.push(listener);
         }
 
-        public RemoveEventListener(f: (value: T) => void): void {
-            if (this.listeners.indexOf(f) >= 0) {
-                this.listeners.splice(this.listeners.indexOf(f), 1);
+        public RemoveEventListener(listener: (values: Array<any>) => void): void {
+            if (this.listeners.indexOf(listener) >= 0) {
+                this.listeners.splice(this.listeners.indexOf(listener), 1);
             }
         }
 
-        public Invoke(value: T): void {
-            this.listeners.forEach(callback => { callback(value); });
+        public Invoke(...values: Array<any>): void {
+            this.listeners.forEach(listener => { listener(values); });
         }
     }
 }
