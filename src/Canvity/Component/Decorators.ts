08c41256687f90575e97b3d2bf4ee06f9b4d1a55
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-07-04 12:52:23 +0100

    Fixed Decorator and added Unique Decorator

--- src/Canvity/Component/Decorators.ts
+++ src/Canvity/Component/Decorators.ts
@@ -1,13 +1,52 @@
 namespace Canvity.Component {
     export function Requires(...types: Array<{ new (...args: any[]): CanvasComponent; }>) {
-        return function (target: any) {
+        return function (target: Function) {
+            let original: Function = target;
+
             let requires = new Array<{ new (...args: any[]): CanvasComponent; }>();
             types.forEach(type => { requires.push(type); });
 
-            Object.defineProperty(target, 'Requires', {
-                value: requires,
+            let newConstructor: any = (...args: any[]) => {
+                var construct: any = function () {
+                    return original.apply(this, args);
+                }
+                construct.prototype = original.prototype;
+                
+                let obj = new construct();
+                Object.defineProperty(obj, 'requires', {
+                    value: requires,
+                    configurable: false,
+                    writable: false
+                });
+                return obj;
+            }
+
+            newConstructor.prototype = original.prototype;
+
+            return newConstructor;
+        }
+    }
+
+    export function Unique(target: Function) {
+        let original: Function = target;
+
+        let newConstructor: any = (...args: any[]) => {
+            var construct: any = function () {
+                return original.apply(this, args);
+            }
+            construct.prototype = original.prototype;
+            
+            let obj = new construct();
+            Object.defineProperty(obj, 'unique', {
+                value: true,
+                configurable: false,
                 writable: false
             });
+            return obj;
         }
+
+        newConstructor.prototype = original.prototype;
+
+        return newConstructor;
     }
 }
