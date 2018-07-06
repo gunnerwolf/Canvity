6804fd8883b0b9e5fc873973a659fc31e3641baa
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-07-06 11:04:37 +0100

    Added time scales

--- src/Canvity/App.ts
+++ src/Canvity/App.ts
@@ -1,18 +1,30 @@
 namespace Canvity {
     export abstract class App {
+        protected timeScale: number;
+
         protected drawInterval: number;
         protected updateInterval: number;
         public set DrawInterval(val: number) { this.drawInterval = val; }
         public set UpdateInterval(val: number) { this.updateInterval = val; }
 
         protected startTime: number;
+        public get ActualRuntime(): number { return (new Date().getTime() / 1000) - this.startTime; }
 
+        private runtime: number;
         public get Runtime(): number { return (new Date().getTime() / 1000) - this.startTime; }
         
         private lastDraw: number;
         private lastUpdate: number;
 
+        private pausedTimeScale: number;
+
+        private paused: boolean;
+        public get Paused(): boolean { return this.paused; }
+
         public constructor(canvas: HTMLCanvasElement) {
+            this.pausedTimeScale = 0;
+            this.timeScale = 1;
+            this.paused = false;
             this.startTime = new Date().getTime() / 1000;
             this.lastDraw = this.startTime;
             this.lastUpdate = this.startTime;
@@ -28,7 +40,7 @@ namespace Canvity {
         public Draw(): void {
             let timestamp = new Date().getTime() / 1000;
             let deltaTime = timestamp - this.lastDraw;
-            let time = new Util.Time(this.Runtime, deltaTime);
+            let time = new Util.Time(this.Runtime, this.ActualRuntime, deltaTime, this.timeScale);
 
             this.lastDraw = timestamp;
             CanvasManager.Draw(time);
@@ -36,10 +48,24 @@ namespace Canvity {
         public Update(): void {
             let timestamp = new Date().getTime() / 1000;
             let deltaTime = timestamp - this.lastUpdate;
-            let time = new Util.Time(this.Runtime, deltaTime);
+            this.runtime += deltaTime * this.timeScale;
+            let time = new Util.Time(this.Runtime, this.ActualRuntime, deltaTime, this.timeScale);
 
             this.lastUpdate = timestamp;
             CanvasManager.Update(time);
+        }
+
+        public Pause(): void {
+            if (this.paused) return;
+            this.pausedTimeScale = this.timeScale;
+            this.timeScale = 0;
+            this.paused = true;
+        }
+        public Unpause(): void {
+            if (!this.paused) return;
+            this.timeScale = this.pausedTimeScale;
+            this.pausedTimeScale = 0;
+            this.paused = false;
         }
     }
 }
--- src/Canvity/Util/Time.ts
+++ src/Canvity/Util/Time.ts
@@ -1,14 +1,22 @@
 namespace Canvity.Util {
     export class Time {
         private deltaTime: number;
-        public get DeltaTime(): number { return this.deltaTime; }
+        public get DeltaTime(): number { return this.deltaTime * this.TimeScale; }
+        public get ActualDeltaTime(): number { return this.deltaTime; }
 
         private time: number;
         public get Time(): number { return this.time; }
+        private actualTime: number;
+        public get ActualTime(): number { return this.actualTime; }
 
-        public constructor(time: number, deltaTime: number) {
+        private timeScale: number;
+        public get TimeScale(): number{ return this.timeScale; }
+
+        public constructor(time: number, realTime: number, deltaTime: number, timeScale: number) {
             this.time = time;
+            this.actualTime = realTime;
             this.deltaTime = deltaTime;
+            this.timeScale = timeScale;
         }
     }
 }
