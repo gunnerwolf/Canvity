0e251135c7805f4d3cb5c58f1f0a7d150753b3d0
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-07-06 16:53:28 +0100

    Reverted HashSet extending Array
    It seems that due to array being a primitive JS type, adding to or modifying the prototype breaks things

--- src/Canvity/Util/HashSet.ts
+++ src/Canvity/Util/HashSet.ts
@@ -2,9 +2,11 @@ namespace Canvity.Util {
     /**
      * Represents an unordered collection of unique items of type T
      */
-    export class HashSet<T> extends Array<T> {
+    export class HashSet<T> {
+        private container: Array<T>;
+
         public constructor(...items: Array<T>) {
-            super(...items);
+            this.container = items;
         }
 
         /**
@@ -14,7 +16,7 @@ namespace Canvity.Util {
          */
         public Add(item: T): boolean {
             if (this.Contains(item)) return false;
-            super.push(item);
+            this.container.push(item);
             return true;
         }
         /**
@@ -24,9 +26,16 @@ namespace Canvity.Util {
          */
         public Remove(item: T): boolean {
             if (!this.Contains(item)) return false;
-            let index: number = super.indexOf(item);
-            super.splice(index, 1);
+            let index: number = this.container.indexOf(item);
+            this.container.splice(index, 1);
             return true;
+        }
+
+        /**
+         * @return number The number of items in the collection
+         */
+        public Count(): number {
+            return this.container.length;
         }
 
         /**
@@ -35,17 +44,17 @@ namespace Canvity.Util {
          * @return boolean Whether the item exists in the collection
          */
         public Contains(item: T): boolean {
-            return super.indexOf(item) >= 0;
+            return this.container.indexOf(item) >= 0;
         }
 
-        /**
-         * Returns the elements of a HashSet that meet the condition specified in a callback function
-         * @param callbackfn A function that accepts up to three arguments. The filter method calls the callbackfn function once for each element in the HashSet
-         * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value
-         * @return A HashSet containing the results of the filter function
-         */
-        public filter(callbackfn: (value: T, index: number, array: T[]) => any, thisArg?: any): HashSet<T> {
-            return HashSet.FromArray(super.filter(callbackfn, thisArg));
+        public forEach(f: (value: T, index: number, array: T[]) => void, thisArg?: any): void { 
+            this.container.forEach(f, thisArg);
+        }
+        public filter(f: (value: T, index: number, array: T[]) => any, thisArg?: any): HashSet<T> {
+            return HashSet.FromArray(this.container.filter(f, thisArg));
+        }
+        public map<U>(f: (value: T, index: number, array: T[]) => U, thisArg?: any): HashSet<U> {
+            return HashSet.FromArray(this.container.map(f, thisArg));;
         }
 
         /**
@@ -53,7 +62,7 @@ namespace Canvity.Util {
          * @return The contents of the HashSet in an ordered Array
          */
         public ToArray(): Array<T> {
-            return <Array<T>>this;
+            return this.container;
         }
 
         /**
@@ -62,7 +71,8 @@ namespace Canvity.Util {
          * @return The contents of the Array in a HashSet
          */
         public static FromArray<T>(array: Array<T>): HashSet<T> {
-            let set = new HashSet<T>(...array);
+            let set = new HashSet<T>();
+            set.container = array;
             return set;
         }
     }
