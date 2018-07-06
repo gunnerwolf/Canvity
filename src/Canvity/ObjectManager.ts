2dbe49e3446ed61e68b5d9a760ec1dd322634d98
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-07-06 14:15:42 +0100

    Added UUID generation to ObjectManager

--- src/Canvity/ObjectManager.ts
+++ src/Canvity/ObjectManager.ts
@@ -1,8 +1,19 @@
 namespace Canvity {
     export class ObjectManager {
         public static GenerateID(): string {
-            // TODO: Generate GUID
-            return '';
+            return ObjectManager.generateUUID();
+        }
+
+        private static generateUUID() {
+            let date = new Date().getTime();
+            if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+                date += performance.now(); //use high-precision timer if available
+            }
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                var r = (date + Math.random() * 16) % 16 | 0;
+                date = Math.floor(date / 16);
+                return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+            });
         }
     }
 }
