2365f28e86edc4d6a73f32c5b3bbe4a0ad8a0a48
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-07-06 10:50:21 +0100

    Changed static singletons to use explicit class references

--- src/Canvity/CanvasManager.ts
+++ src/Canvity/CanvasManager.ts
@@ -1,40 +1,40 @@
 namespace Canvity {
     export class CanvasManager {
         private static canvas: HTMLCanvasElement;
-        public static get Canvas(): HTMLCanvasElement { return this.canvas; }
+        public static get Canvas(): HTMLCanvasElement { return CanvasManager.canvas; }
         
         private static ctx: CanvasRenderingContext2D;
-        public static get Context(): CanvasRenderingContext2D { return this.ctx; }
+        public static get Context(): CanvasRenderingContext2D { return CanvasManager.ctx; }
 
         private static scenes: Util.HashSet<CanvasScene>;
         private static currentScene: CanvasScene;
 
         public static Init(canvas: HTMLCanvasElement): void {
-            this.canvas = canvas;
+            CanvasManager.canvas = canvas;
             let ctx = canvas.getContext('2d');
-            if (ctx !== null) this.ctx = ctx;
+            if (ctx !== null) CanvasManager.ctx = ctx;
 
-            this.scenes = new Util.HashSet<CanvasScene>();
+            CanvasManager.scenes = new Util.HashSet<CanvasScene>();
         }
 
         public static AddScene(scene: CanvasScene): void {
-            if (!this.scenes.Add(scene)) throw Error('Attempted to add Scene that was already added!');
+            if (!CanvasManager.scenes.Add(scene)) throw Error('Attempted to add Scene that was already added!');
         }
         public static RemoveScene(scene: CanvasScene): void {
-            this.scenes.Remove(scene);
+            CanvasManager.scenes.Remove(scene);
         }
         public static SwitchScene(scene: CanvasScene): void {
-            if (!this.scenes.Contains(scene)) throw Error('Attempted to switch to Scene that does not exist!');
-            this.currentScene = scene;
+            if (!CanvasManager.scenes.Contains(scene)) throw Error('Attempted to switch to Scene that does not exist!');
+            CanvasManager.currentScene = scene;
         }
 
         public static Draw(time: Util.Time) {
-            if (this.currentScene === null || this.currentScene === undefined) return;
-            this.currentScene.Draw(time, this.ctx);
+            if (CanvasManager.currentScene === null || CanvasManager.currentScene === undefined) return;
+            CanvasManager.currentScene.Draw(time, CanvasManager.ctx);
         }
         public static Update(time: Util.Time) {
-            if (this.currentScene === null || this.currentScene === undefined) return;
-            this.currentScene.Update(time);
+            if (CanvasManager.currentScene === null || CanvasManager.currentScene === undefined) return;
+            CanvasManager.currentScene.Update(time);
         }
     }
 }
--- src/Canvity/InputManager.ts
+++ src/Canvity/InputManager.ts
@@ -1,58 +1,62 @@
 namespace Canvity {
     export class InputManager {
         private static mousePos: Util.Vector2;
-        public static get MousePos(): Util.Vector2 { return this.mousePos; }
+        public static get MousePos(): Util.Vector2 { return InputManager.mousePos; }
 
         private static mouseDelta: Util.Vector2;
-        public static get MouseDelta(): Util.Vector2 { return this.mouseDelta; }
+        public static get MouseDelta(): Util.Vector2 { return InputManager.mouseDelta; }
 
         private static onMouseMove: Events.CanvityEvent;
-        public static get OnMouseMove(): Events.CanvityEvent { return this.onMouseMove; }
+        public static get OnMouseMove(): Events.CanvityEvent { return InputManager.onMouseMove; }
         private static onMouseDown: Events.CanvityEvent;
-        public static get OnMouseDown(): Events.CanvityEvent { return this.onMouseDown; }
+        public static get OnMouseDown(): Events.CanvityEvent { return InputManager.onMouseDown; }
         private static onMouseUp: Events.CanvityEvent;
-        public static get OnMouseUp(): Events.CanvityEvent { return this.onMouseUp; }
+        public static get OnMouseUp(): Events.CanvityEvent { return InputManager.onMouseUp; }
 
         private static downButtons: number;
-        public static get IsLeftButtonDown(): boolean { return (this.downButtons & 1) !== 0; }
-        public static get IsRightButtonDown(): boolean { return (this.downButtons & 2) !== 0; }
-        public static get IsMiddleButtonDown(): boolean { return (this.downButtons & 4) !== 0; }
+        public static get IsLeftButtonDown(): boolean { return (InputManager.downButtons & 1) !== 0; }
+        public static get IsRightButtonDown(): boolean { return (InputManager.downButtons & 2) !== 0; }
+        public static get IsMiddleButtonDown(): boolean { return (InputManager.downButtons & 4) !== 0; }
 
         public static Init(): void {
-            this.downButtons = 0;
+            InputManager.downButtons = 0;
 
-            document.addEventListener('mousemove', this.HandleMouseMove);
-            document.addEventListener('mousedown', this.HandleMouseDown);
-            document.addEventListener('mouseup', this.HandleMouseUp);
+            InputManager.onMouseMove = new Events.CanvityEvent();
+            InputManager.onMouseDown = new Events.CanvityEvent();
+            InputManager.onMouseUp = new Events.CanvityEvent();
+
+            document.addEventListener('mousemove', InputManager.HandleMouseMove);
+            document.addEventListener('mousedown', InputManager.HandleMouseDown);
+            document.addEventListener('mouseup', InputManager.HandleMouseUp);
         }
 
         public static HandleMouseMove(mouse: MouseEvent): void {
             mouse.preventDefault();
 
-            this.mousePos = new Util.Vector2(mouse.pageX, mouse.pageY);
-            this.mouseDelta = new Util.Vector2(mouse.movementX, mouse.movementY);
+            InputManager.mousePos = new Util.Vector2(mouse.pageX, mouse.pageY);
+            InputManager.mouseDelta = new Util.Vector2(mouse.movementX, mouse.movementY);
 
-            this.OnMouseMove.Invoke();
+            InputManager.OnMouseMove.Invoke();
         }
 
         public static HandleMouseDown(mouse: MouseEvent): void {
             mouse.preventDefault();
 
-            let pressed: number = mouse.buttons & ~this.downButtons;
+            let pressed: number = mouse.buttons & ~InputManager.downButtons;
 
-            this.downButtons = mouse.buttons;
+            InputManager.downButtons = mouse.buttons;
 
-            this.OnMouseDown.Invoke(pressed);
+            InputManager.OnMouseDown.Invoke(pressed);
         }
 
         public static HandleMouseUp(mouse: MouseEvent): void {
             mouse.preventDefault();
             
-            let released: number = this.downButtons & ~mouse.buttons
+            let released: number = InputManager.downButtons & ~mouse.buttons
 
-            this.downButtons = mouse.buttons;
+            InputManager.downButtons = mouse.buttons;
 
-            this.OnMouseUp.Invoke(released);
+            InputManager.OnMouseUp.Invoke(released);
         }
     }
 }
