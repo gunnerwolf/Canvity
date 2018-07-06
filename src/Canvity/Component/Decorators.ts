e4a69cf9f8815ce0f3a146a2ad30e010b05249bf
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-07-06 13:36:48 +0100

    Fixed decorators not properly following class inheritance

--- src/Canvity/Component/Decorators.ts
+++ src/Canvity/Component/Decorators.ts
@@ -1,52 +1,31 @@
 namespace Canvity.Component {
     export function Requires(...types: Array<{ new (...args: any[]): CanvasComponent; }>) {
-        return function (target: Function) {
-            let original: Function = target;
-
+        return function<T extends new (...arg: any[])=> any> (target: T) {
             let requires = new Array<{ new (...args: any[]): CanvasComponent; }>();
             types.forEach(type => { requires.push(type); });
 
-            let newConstructor: any = (...args: any[]) => {
-                var construct: any = function () {
-                    return original.apply(this, args);
+            return class extends target {
+                constructor(...args: any[]) {
+                    super(...args);
+                    Object.defineProperty(this, 'requires', {
+                        value: requires,
+                        configurable: false,
+                        writable: false
+                    });
                 }
-                construct.prototype = original.prototype;
-                
-                let obj = new construct();
-                Object.defineProperty(obj, 'requires', {
-                    value: requires,
+            }
+        }
+    }
+    export function Unique<T extends new (...arg: any[])=> any>(target: T) {    
+        return class extends target {
+            constructor(...args: any[]){
+                super(...args);
+                Object.defineProperty(this, 'unique', {
+                    value: true,
                     configurable: false,
                     writable: false
                 });
-                return obj;
             }
-
-            newConstructor.prototype = original.prototype;
-
-            return newConstructor;
-        }
-    }
-
-    export function Unique(target: Function) {
-        let original: Function = target;
-
-        let newConstructor: any = (...args: any[]) => {
-            var construct: any = function () {
-                return original.apply(this, args);
-            }
-            construct.prototype = original.prototype;
-            
-            let obj = new construct();
-            Object.defineProperty(obj, 'unique', {
-                value: true,
-                configurable: false,
-                writable: false
-            });
-            return obj;
-        }
-
-        newConstructor.prototype = original.prototype;
-
-        return newConstructor;
+        };
     }
 }
