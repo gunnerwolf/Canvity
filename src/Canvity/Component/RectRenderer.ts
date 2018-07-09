91506ac575db1509811d32f71a8b3f9cff07fd4e
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-07-09 17:31:14 +0100

    Fixed RectRenderer always using Transform's rect when available

--- src/Canvity/Component/RectRenderer.ts
+++ src/Canvity/Component/RectRenderer.ts
@@ -7,23 +7,17 @@ namespace Canvity.Component {
 
         private rect: Util.Rect;
         public get Rect(): Util.Rect {
-            if (!(this.Transform instanceof RectTransform)) {
+            if (!this.rect) {
+                return (<RectTransform>this.Transform).Rect;
+            } else {
                 let pos: Util.Vector2 = new Util.Vector2();
                 if (this.Transform !== null) {
                     pos = this.Transform.Position;
                 }
                 return new Util.Rect(pos.X + this.rect.X, pos.Y + this.rect.Y, this.rect.W, this.rect.H);
-            } else {
-                return (<RectTransform>this.Transform).Rect;
             }
         }
-        public set Rect(val: Util.Rect) {
-            if (!(this.Transform instanceof RectTransform)) {
-                this.rect = val;
-            } else {
-                (<RectTransform>this.Transform).Rect = val;
-            }
-        }
+        public set Rect(val: Util.Rect) { this.rect = val; }
 
         public constructor(color: Util.Color) {
             super();
