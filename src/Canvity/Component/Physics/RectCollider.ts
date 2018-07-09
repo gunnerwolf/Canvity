ace1c7aea8b9a786e1b94bbaa3719e9fda7873ce
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-07-09 11:05:45 +0100

    Fixed infinite recursion in RectCollider

--- src/Canvity/Component/Physics/RectCollider.ts
+++ src/Canvity/Component/Physics/RectCollider.ts
@@ -4,10 +4,13 @@ namespace Canvity.Component.Physics {
         public get Rect(): Util.Rect { return this.BoundingBox; }
         public set Rect(val: Util.Rect) { this.vertices = val.Vertices; }
 
-        public get Vertices(): Array<Util.Vector2> { return this.Rect.Vertices; }
-
-        public constructor() {
+        public constructor(rect: Util.Rect) {
             super();
+
+            this.Vertices.push(new Util.Vector2(rect.X, rect.Y));
+            this.Vertices.push(new Util.Vector2(rect.X + rect.W, rect.Y));
+            this.Vertices.push(new Util.Vector2(rect.X + rect.W, rect.Y + rect.H));
+            this.Vertices.push(new Util.Vector2(rect.X, rect.Y + rect.H));
         }
 
         public CheckIsCollision(point: Util.Vector2): boolean {
