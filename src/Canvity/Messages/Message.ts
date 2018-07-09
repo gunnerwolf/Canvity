dd7eab6b054d17dd03528c4f6645e7836bab8edf
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-07-09 14:28:10 +0100

    Added Data field to Message

--- src/Canvity/Messages/Message.ts
+++ src/Canvity/Messages/Message.ts
@@ -3,11 +3,13 @@ namespace Canvity.Messages {
         public Time: Util.Time;
         public Message: string;
         public Target: string;
+        public Data: Array<any>;
 
-        public constructor(time: Util.Time, message: string, target: string) {
+        public constructor(time: Util.Time, message: string, target: string, ...data: Array<any>) {
             this.Time = time;
             this.Message = message;
             this.Target = target;
+            this.Data = data;
         }
     }
 }
