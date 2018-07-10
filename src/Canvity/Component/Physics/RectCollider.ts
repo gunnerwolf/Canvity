c143018d8ed3419a18f47a07edde8bdffe8adb67
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-07-10 12:19:58 +0100

    Fixed MoveBar's RectCollider using absolute positioning

--- src/Canvity/Component/Physics/RectCollider.ts
+++ src/Canvity/Component/Physics/RectCollider.ts
@@ -2,7 +2,7 @@ namespace Canvity.Component.Physics {
     export class RectCollider extends PolygonCollider {
         protected rect: Util.Rect;
         public get Rect(): Util.Rect { return this.BoundingBox; }
-        public set Rect(val: Util.Rect) { this.vertices = val.Vertices; }
+        public set Rect(val: Util.Rect) { this.Vertices = val.Vertices; }
 
         private fetchRect: boolean;
 
