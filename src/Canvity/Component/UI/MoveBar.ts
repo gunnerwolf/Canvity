8dd8719c98c2aecaad93feef903b98c99e6ef0c2
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-07-16 09:22:21 +0100

    Refactored MoveBar to only handle dragging logic

--- src/Canvity/Component/UI/MoveBar.ts
+++ src/Canvity/Component/UI/MoveBar.ts
@@ -1,33 +1,15 @@
 namespace Canvity.Component.UI {
-    @Canvity.Component.Requires(UI.Window, Physics.RectCollider)
-    export class MoveBar extends Graphics.RectRenderer {
-        private height: number;
-        public get Height(): number { return this.height; }
-        public set Height(val: number) { this.height = val; }
-
-        private baseColor: Util.Color;
-
+    // TODO: Refactor into smaller, more generic components
+    @Canvity.Component.Requires(Physics.RectCollider)
+    export class MoveBar extends CanvasComponent {
         private isDragging: boolean;
 
         private dragStart: Util.Vector2;
 
         private collider: Physics.RectCollider
 
-        public constructor(height: number) {
-            super(new Util.Color(255, 255, 255, 150));
-
-            this.height = height;
-        }
-
-        public Draw(deltaTime: Util.Time, ctx: CanvasRenderingContext2D): void {
-            ctx.fillStyle = this.baseColor.CssString;
-            ctx.fillRect(this.Rect.X, this.Rect.Y, this.Rect.W, this.height);
-
-            super.Draw(deltaTime, ctx);
-
-            ctx.strokeStyle = this.baseColor.CssString;
-            ctx.lineWidth = 2;
-            ctx.strokeRect(this.Rect.X, this.Rect.Y, this.Rect.W, this.height);
+        public constructor() {
+            super();
         }
 
         public Update(deltaTime: Util.Time): void {
@@ -36,25 +18,10 @@ namespace Canvity.Component.UI {
             Messaging.MessageBus.GetGlobalMessages('input').forEach(message => { this.handleInputMessage(message); }, this);
         }
 
-        public GetRequiredComponents(obj: CanvasObject): Array<CanvasComponent> {
-            let rect = new Physics.RectCollider(new Util.Rect());
-            this.collider = rect;
-            let compArr = new Array<CanvasComponent>(rect);
-            if (!obj.HasComponent(UI.Window)) {
-                let window = <Window>obj.AddComponent(new Window(Util.Color.Black), true);
-                compArr.unshift(window);
-            }
-            return compArr;
-        }
-
         protected onParentSet(): void {
-            let window: UI.Window | null = this.CanvasObject.GetComponent(UI.Window);
-            if (window !== null) {
-                let windowRect: Util.Rect = window.Rect;
-                this.Rect = new Util.Rect(0, 0, windowRect.W, this.height);
-                this.collider.Rect = this.Rect;
-                this.baseColor = window.Color;
-            }
+            let collider: Physics.RectCollider | null =  this.CanvasObject.GetComponent(Physics.RectCollider);
+            if (collider === null) throw Error("Attempted to add component MoveBar to an object that does not contain an instance of RectCollider");
+            else this.collider = collider;
         }
 
         protected onMouseDown() {
@@ -70,8 +37,7 @@ namespace Canvity.Component.UI {
         }
         protected onMouseMove() {
             if (this.isDragging) {
-                let transform: Transform | null = this.Transform;
-                if (transform === null) return;
+                let transform: Transform = this.Transform;
                 
                 transform.LocalPosition = transform.LocalPosition.Add(InputManager.MousePos.Sub(this.dragStart));
                 this.dragStart = InputManager.MousePos;
@@ -80,15 +46,7 @@ namespace Canvity.Component.UI {
 
         protected handleObjectMessage(message: Messaging.Message): void {
             let messageParts: Array<string> = message.Message.split('.');
-            if (messageParts[0] === 'transform') {
-                if (messageParts[1] === 'resize') {
-                    let transform: Transform | null = this.Transform;
-                    if (transform !== null) {
-                        this.Rect = new Util.Rect(0, 0, (<RectTransform>transform).Rect.W, this.Height);
-                        this.collider.Rect = this.Rect;
-                    }
-                }
-            } else if (messageParts[0] === 'collider') {
+            if (messageParts[0] === 'collider') {
                 if (message.Data[0] !== this.collider) return;
                 if (messageParts[1] !== 'mouse') return;
                 if (messageParts[2] === 'down') this.onMouseDown();
