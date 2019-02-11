9b9943e6159768e3b014bbc6f6bd1c3398104c91
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-02-11 14:36:58 -0500

    Added webpack, cleaned up gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,32 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+const rimraf = require('rimraf');
 
-gulp.task('build', function() {
+const webpack = require('webpack-stream');
+const webpackConf = require('./webpack.config.js');
+
+function build() {
     var tsProject = ts.createProject('tsconfig.json');
 
     var tsResult = tsProject.src()
         .pipe(tsProject()).js
         .pipe(gulp.dest('bin'));
-});
+
+    return tsResult;
+};
+
+function pack() {
+    return gulp.src('bin/js/**/*.js')
+        .pipe(webpack(webpackConf))
+        .pipe(gulp.dest('bin/pack'));
+};
+
+function clean(done) {
+    rimraf.sync('./bin');
+    done();
+}
+
+exports.build = build;
+exports.pack = pack;
+exports.clean = clean;
+exports.default = gulp.series(clean, build, pack);
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    entry: './bin/Canvity/Main.js',
+    mode: 'production',
+    output: {
+        filename: 'Canvity.js'
+    }
+}
