ff2164eff308c9c65dc5f6a2c573a7f34b3e94af
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-02-13 09:56:10 -0500

    Added static methods to load assets asynchronously

--- src/Canvity/Assets/Asset.ts
+++ src/Canvity/Assets/Asset.ts
@@ -1,5 +1,8 @@
 namespace Canvity.Assets {
     export abstract class Asset {
+        private static assets: Array<Asset>;
+        public static get Assets(): Array<Asset> { return Asset.assets; }
+
         protected assetName: string;
         public get AssetName(): string { return this.assetName; }
 
@@ -9,6 +12,30 @@ namespace Canvity.Assets {
         public constructor(name: string, path: string = "") {
             this.assetName = name;
             this.filePath = path;
+        }
+
+        public abstract parseAsset(httpReq: XMLHttpRequest): void;
+
+        public static loadAssetFromURI<T extends Asset>(c: {new(name: string, path: string): T; }, uri: string, assetName: string): void {
+            let httpReq = new XMLHttpRequest();
+
+            httpReq.onreadystatechange = () => {
+                Asset.parseLoadedAsset(c, httpReq, assetName);
+            };
+
+            httpReq.open('GET', uri, true);
+            httpReq.send();
+        }
+
+        public static addLoadedAsset(asset: Asset): void {
+            Asset.assets.push(asset);
+        }
+
+        private static parseLoadedAsset<T extends Asset>(c: {new(name: string, path: string): T; }, httpReq: XMLHttpRequest, assetName: string): void {
+            let asset = new c(assetName, httpReq.responseURL);
+            asset.parseAsset(httpReq);
+
+            Asset.addLoadedAsset(asset);
         }
     }
 }
