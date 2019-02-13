7314fe33b57ee486367813fd3a9ea0a2eee05f27
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-02-13 15:47:07 -0500

    Added ts-loader for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
 module.exports = {
-    entry: './bin/Canvity/Main.js',
-    mode: 'production',
+    entry: './src/Canvity/Main.ts',
+    devtool: 'inline-source-map',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ]
+    },
     output: {
         filename: 'Canvity.js'
     }
