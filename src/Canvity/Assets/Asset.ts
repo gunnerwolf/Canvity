0dc792a9f4fabc88492777ea4efdf1443e417272
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-02-13 09:54:58 -0500

    Restructured Asset constructor

--- src/Canvity/Assets/Asset.ts
+++ src/Canvity/Assets/Asset.ts
@@ -1,9 +1,13 @@
 namespace Canvity.Assets {
     export abstract class Asset {
+        protected assetName: string;
+        public get AssetName(): string { return this.assetName; }
+
         protected filePath: string;
         public get FilePath(): string { return this.filePath; }
 
-        public constructor(path: string) {
+        public constructor(name: string, path: string = "") {
+            this.assetName = name;
             this.filePath = path;
         }
     }
