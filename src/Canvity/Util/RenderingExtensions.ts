e27ee966c8c6d99107dff18d78b21aa8caf4869c
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-02-19 14:21:45 -0500

    Deleted RenderingExtensions

--- src/Canvity/Util/RenderingExtensions.ts
+++
@@ -1,7 +0,0 @@
-interface CanvasRenderingContext2D {
-    drawSprite(sprite: Canvity.Assets.Sprite, x: number, y: number): void;
-}
-
-CanvasRenderingContext2D.prototype.drawSprite = (sprite: Canvity.Assets.Sprite, x: number, y: number) => {
-    this.drawImage(sprite.Image, x, y);
-}
