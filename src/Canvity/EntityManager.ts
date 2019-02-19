1dc5d1e44010ca21bf43b470ac5b8515b36f0779
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-02-19 13:42:26 -0500

    Changed EntityManager to static

--- src/Canvity/EntityManager.ts
+++ src/Canvity/EntityManager.ts
@@ -1,8 +1,8 @@
 import { IComponentManager } from './Component/IComponentManager';
 
 export class EntityManager {
-    public createEntity(... components: IComponentManager[]): number {
-        let entityID = this.generateID();
+    public static createEntity(... components: IComponentManager[]): number {
+        let entityID = EntityManager.generateID();
 
         components.forEach(component => {
             component.createComponent(entityID);
@@ -11,14 +11,14 @@ export class EntityManager {
         return entityID;
     }
 
-    private readonly ID_LENGTH = 16;
+    private static readonly ID_LENGTH = 16;
 
-    private generateID(): number {
+    private static generateID(): number {
         let out = 0;
-        for(let i = 0; i < this.ID_LENGTH - 1; i++) {
+        for(let i = 0; i < EntityManager.ID_LENGTH - 1; i++) {
             out += Math.floor((Math.random() * 10)) * Math.pow(10, i);
         }
-        out += Math.floor((Math.random() * 9) + 1) * Math.pow(10, this.ID_LENGTH - 1);
+        out += Math.floor((Math.random() * 9) + 1) * Math.pow(10, EntityManager.ID_LENGTH - 1);
 
         return out;
     }
