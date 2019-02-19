eaf5b61821e40f12be1e435b7af740508f9bbab7
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-02-19 14:15:26 -0500

    Refactored App

--- src/Canvity/App.ts
+++ src/Canvity/App.ts
@@ -1,111 +1,113 @@
-namespace Canvity {
-    export abstract class App {
-        public static CurrentUpdateTime: Util.Time;
-        public static CurrentDrawTime: Util.Time;
+import { Time } from "./Util/Time";
+import { IRenderingContext } from "./Render/IRenderingContext";
+import { RenderingContext2D } from "./Render/RenderingContext2D";
+import { RenderingContextWebGL } from "./Render/RenderingContextWebGL";
+import { InputManager } from "./InputManager";
+import { CanvasManager } from "./CanvasManager";
 
-        protected timeScale: number;
+export abstract class App {
+    public static CurrentUpdateTime: Time;
+    public static CurrentDrawTime: Time;
 
-        protected drawInterval: number;
-        protected updateInterval: number;
-        public set DrawInterval(val: number) { this.drawInterval = val; }
-        public set UpdateInterval(val: number) { this.updateInterval = val; }
+    protected timeScale: number;
 
-        protected startTime: number;
-        public get ActualRuntime(): number { return (new Date().getTime() / 1000) - this.startTime; }
+    protected drawInterval: number;
+    protected updateInterval: number;
+    public set DrawInterval(val: number) { this.drawInterval = val; }
+    public set UpdateInterval(val: number) { this.updateInterval = val; }
 
-        private runtime: number;
-        public get Runtime(): number { return (new Date().getTime() / 1000) - this.startTime; }
-        
-        private lastDraw: number;
-        private lastUpdate: number;
+    protected startTime: number;
+    public get ActualRuntime(): number { return (new Date().getTime() / 1000) - this.startTime; }
 
-        private pausedTimeScale: number;
+    private runtime: number;
+    public get Runtime(): number { return (new Date().getTime() / 1000) - this.startTime; }
+    
+    private lastDraw: number;
+    private lastUpdate: number;
 
-        private canvas: HTMLCanvasElement;
-        private ctx: Render.IRenderingContext;
+    private pausedTimeScale: number;
 
-        private paused: boolean;
-        public get Paused(): boolean { return this.paused; }
+    private canvas: HTMLCanvasElement;
+    private ctx: IRenderingContext;
 
-        private static instance: App;
-        public static get renderContext(): Render.IRenderingContext { return App.instance.ctx; }
-        public static get renderContext2d(): Render.RenderingContext2D | null {
-            if (App.renderContext instanceof Render.RenderingContext2D)
-                return <Render.RenderingContext2D> App.renderContext;
-            return null;
+    private paused: boolean;
+    public get Paused(): boolean { return this.paused; }
+
+    private static instance: App;
+    public static get renderContext(): IRenderingContext { return App.instance.ctx; }
+    public static get renderContext2d(): RenderingContext2D | null {
+        if (App.renderContext instanceof RenderingContext2D)
+            return <RenderingContext2D> App.renderContext;
+        return null;
+    }
+    public static get renderContextWebGL(): RenderingContextWebGL | null {
+        if (App.renderContext instanceof RenderingContextWebGL)
+            return <RenderingContextWebGL> App.renderContext;
+        return null;
+    }
+
+    public constructor(canvas: HTMLCanvasElement) {
+        this.pausedTimeScale = 0;
+        this.timeScale = 1;
+        this.paused = false;
+        this.startTime = new Date().getTime() / 1000;
+        this.lastDraw = this.startTime;
+        this.lastUpdate = this.startTime;
+
+        this.canvas = canvas;
+        App.instance = this;
+    }
+
+    public PreInit(opts: any): void {
+        if (!opts.renderTarget) opts.renderTarget = '2d';
+        switch(opts.renderTarget.toLowerCase()) {
+            case '2d':
+                this.ctx = new RenderingContext2D(this.canvas);
+                break;
+            case 'gl':
+            case 'webgl':
+            case 'opengl':
+                this.ctx = new RenderingContextWebGL(this.canvas);
+                break;
         }
-        public static get renderContextWebGL(): Render.RenderingContextWebGL | null {
-            if (App.renderContext instanceof Render.RenderingContextWebGL)
-                return <Render.RenderingContextWebGL> App.renderContext;
-            return null;
-        }
+    }
+    public Init(drawDeltaTime: number, updateDeltaTime: number): void
+    {
+        InputManager.Init();
+        CanvasManager.Init(this.canvas, this.ctx);
+    }
+    public PostInit(): void { }
 
-        public constructor(canvas: HTMLCanvasElement) {
-            this.pausedTimeScale = 0;
-            this.timeScale = 1;
-            this.paused = false;
-            this.startTime = new Date().getTime() / 1000;
-            this.lastDraw = this.startTime;
-            this.lastUpdate = this.startTime;
+    public Draw(): void {
+        let timestamp = new Date().getTime() / 1000;
+        let deltaTime = timestamp - this.lastDraw;
+        let time = new Time(this.Runtime, this.ActualRuntime, deltaTime, this.timeScale);
+        App.CurrentDrawTime = time;
 
-            this.canvas = canvas;
-            App.instance = this;
-        }
+        this.lastDraw = timestamp;
+        CanvasManager.Draw(time);
+    }
+    public Update(): void {
+        let timestamp = new Date().getTime() / 1000;
+        let deltaTime = timestamp - this.lastUpdate;
+        this.runtime += deltaTime * this.timeScale;
+        let time = new Time(this.Runtime, this.ActualRuntime, deltaTime, this.timeScale);
+        App.CurrentUpdateTime = time;
 
-        public PreInit(opts: any): void {
-            if (!opts.renderTarget) opts.renderTarget = '2d';
-            switch(opts.renderTarget.toLowerCase()) {
-                case '2d':
-                    this.ctx = new Render.RenderingContext2D(this.canvas);
-                    break;
-                case 'gl':
-                case 'webgl':
-                case 'opengl':
-                    this.ctx = new Render.RenderingContextWebGL(this.canvas);
-                    break;
-            }
-        }
-        public Init(drawDeltaTime: number, updateDeltaTime: number): void
-        {
-            Messaging.MessageBus.Init();
-            InputManager.Init();
-            CanvasManager.Init(this.canvas, this.ctx);
-        }
-        public PostInit(): void { }
+        this.lastUpdate = timestamp;
+        CanvasManager.Update(time);
+    }
 
-        public Draw(): void {
-            let timestamp = new Date().getTime() / 1000;
-            let deltaTime = timestamp - this.lastDraw;
-            let time = new Util.Time(this.Runtime, this.ActualRuntime, deltaTime, this.timeScale);
-            App.CurrentDrawTime = time;
-
-            this.lastDraw = timestamp;
-            CanvasManager.Draw(time);
-        }
-        public Update(): void {
-            let timestamp = new Date().getTime() / 1000;
-            let deltaTime = timestamp - this.lastUpdate;
-            this.runtime += deltaTime * this.timeScale;
-            let time = new Util.Time(this.Runtime, this.ActualRuntime, deltaTime, this.timeScale);
-            App.CurrentUpdateTime = time;
-
-            this.lastUpdate = timestamp;
-            CanvasManager.Update(time);
-
-            Messaging.MessageBus.ClearMessages();
-        }
-
-        public Pause(): void {
-            if (this.paused) return;
-            this.pausedTimeScale = this.timeScale;
-            this.timeScale = 0;
-            this.paused = true;
-        }
-        public Unpause(): void {
-            if (!this.paused) return;
-            this.timeScale = this.pausedTimeScale;
-            this.pausedTimeScale = 0;
-            this.paused = false;
-        }
+    public Pause(): void {
+        if (this.paused) return;
+        this.pausedTimeScale = this.timeScale;
+        this.timeScale = 0;
+        this.paused = true;
+    }
+    public Unpause(): void {
+        if (!this.paused) return;
+        this.timeScale = this.pausedTimeScale;
+        this.pausedTimeScale = 0;
+        this.paused = false;
     }
 }
