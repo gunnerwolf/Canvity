3fd5aa6eb6ff32eb833dc93eda5f560f148a4f81
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-02-19 14:12:50 -0500

    Refactored CanvasManager

--- src/Canvity/CanvasManager.ts
+++ src/Canvity/CanvasManager.ts
@@ -1,45 +1,47 @@
-namespace Canvity {
-    export class CanvasManager {
-        private static canvas: HTMLCanvasElement;
-        public static get Canvas(): HTMLCanvasElement { return CanvasManager.canvas; }
-        
-        private static ctx: Render.IRenderingContext;
-        public static get Context(): Render.IRenderingContext { return CanvasManager.ctx; }
+import { IRenderingContext } from "./Render/IRenderingContext";
+import { HashSet } from "./Util/HashSet";
+import { CanvasScene } from "./CanvasScene";
 
-        private static scenes: Util.HashSet<CanvasScene>;
-        private static currentScene: CanvasScene;
-        public static get CurrentScene(): CanvasScene { return this.currentScene; }
+export class CanvasManager {
+    private static canvas: HTMLCanvasElement;
+    public static get Canvas(): HTMLCanvasElement { return CanvasManager.canvas; }
+    
+    private static ctx: IRenderingContext;
+    public static get Context(): IRenderingContext { return CanvasManager.ctx; }
 
-        public static Init(canvas: HTMLCanvasElement, ctx: Render.IRenderingContext): void {
-            CanvasManager.canvas = canvas;
-            CanvasManager.ctx = ctx;
-            CanvasManager.scenes = new Util.HashSet<CanvasScene>();
-        }
+    private static scenes: HashSet<CanvasScene>;
+    private static currentScene: CanvasScene;
+    public static get CurrentScene(): CanvasScene { return this.currentScene; }
 
-        public static AddScene(scene: CanvasScene): void {
-            if (!CanvasManager.scenes.Add(scene)) throw Error('Attempted to add Scene that was already added!');
-        }
-        public static RemoveScene(scene: CanvasScene): void {
-            CanvasManager.scenes.Remove(scene);
-        }
-        public static SwitchScene(scene: CanvasScene): void {
-            if (!CanvasManager.scenes.Contains(scene)) throw Error('Attempted to switch to Scene that does not exist!');
-            CanvasManager.currentScene = scene;
-        }
+    public static Init(canvas: HTMLCanvasElement, ctx: IRenderingContext): void {
+        CanvasManager.canvas = canvas;
+        CanvasManager.ctx = ctx;
+        CanvasManager.scenes = new HashSet<CanvasScene>();
+    }
 
-        public static Draw(time: Util.Time) {
-            if (CanvasManager.currentScene === null || CanvasManager.currentScene === undefined) return;
-            CanvasManager.currentScene.Draw(time, CanvasManager.ctx);
-            CanvasManager.ctx.draw(time);
-        }
-        public static Update(time: Util.Time) {
-            if (CanvasManager.currentScene === null || CanvasManager.currentScene === undefined) return;
-            CanvasManager.currentScene.Update(time);
-        }
+    public static AddScene(scene: CanvasScene): void {
+        if (!CanvasManager.scenes.Add(scene)) throw Error('Attempted to add Scene that was already added!');
+    }
+    public static RemoveScene(scene: CanvasScene): void {
+        CanvasManager.scenes.Remove(scene);
+    }
+    public static SwitchScene(scene: CanvasScene): void {
+        if (!CanvasManager.scenes.Contains(scene)) throw Error('Attempted to switch to Scene that does not exist!');
+        CanvasManager.currentScene = scene;
+    }
 
-        public static Start() {
-            if (CanvasManager.currentScene === null || CanvasManager.currentScene === undefined) throw Error("Attempted to start app with no scenes added!");
-            CanvasManager.currentScene.Start();
-        }
+    public static Draw(time: Time) {
+        if (CanvasManager.currentScene === null || CanvasManager.currentScene === undefined) return;
+        CanvasManager.currentScene.Draw(time, CanvasManager.ctx);
+        CanvasManager.ctx.draw(time);
+    }
+    public static Update(time: Time) {
+        if (CanvasManager.currentScene === null || CanvasManager.currentScene === undefined) return;
+        CanvasManager.currentScene.Update(time);
+    }
+
+    public static Start() {
+        if (CanvasManager.currentScene === null || CanvasManager.currentScene === undefined) throw Error("Attempted to start app with no scenes added!");
+        CanvasManager.currentScene.Start();
     }
 }
