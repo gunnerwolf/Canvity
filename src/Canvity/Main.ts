9be1b97d7b91bd3970658f0fd4623e30cc8ae5aa
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-02-19 14:15:32 -0500

    Refactored Main

--- src/Canvity/Main.ts
+++ src/Canvity/Main.ts
@@ -1,27 +1,25 @@
-namespace Canvity {
-    export function StartApp(app: App, opts: any = { renderTarget: '2d', frameRate: 60, fpsLocked: true, updateRate: 60 }): void {
-        app.PreInit(opts);
+import { App } from "./App";
 
-        let drawDeltaTime = 1000 / opts.frameRate;
-        let updateDeltaTime = 1000;
-        if (!opts.fpsLocked) {
-            updateDeltaTime = 1000 / opts.updateRate;
-        }
+export function StartApp(app: App, opts: any = { renderTarget: '2d', frameRate: 60, fpsLocked: true, updateRate: 60 }): void {
+    app.PreInit(opts);
 
-        app.Init(drawDeltaTime, updateDeltaTime);
-
-        if (!opts.fpsLocked) {
-            app.UpdateInterval = setInterval(function() {
-                app.Update();
-            }, updateDeltaTime);
-        }
-        app.DrawInterval = setInterval(function() {
-            if (opts.fpsLocked) app.Update();
-            app.Draw();
-        }, drawDeltaTime);
-
-        CanvasManager.Start();
-
-        app.PostInit();
+    let drawDeltaTime = 1000 / opts.frameRate;
+    let updateDeltaTime = 1000;
+    if (!opts.fpsLocked) {
+        updateDeltaTime = 1000 / opts.updateRate;
     }
+
+    app.Init(drawDeltaTime, updateDeltaTime);
+
+    if (!opts.fpsLocked) {
+        app.UpdateInterval = setInterval(function() {
+            app.Update();
+        }, updateDeltaTime);
+    }
+    app.DrawInterval = setInterval(function() {
+        if (opts.fpsLocked) app.Update();
+        app.Draw();
+    }, drawDeltaTime);
+
+    app.PostInit();
 }
